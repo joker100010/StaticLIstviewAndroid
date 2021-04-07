9dc2b83ca4ac9f22b60a597794b63e99cb78ab44
Hasnain Raza <Hassanain.okz110@gmail.com>
2021-04-07 17:57:21 +0500

    This Dynamnic list view with adapter

--- app/src/main/java/com/example/listviews/MainActivity.java
+++ app/src/main/java/com/example/listviews/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.listviews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String Name [] = {"Hasnain","Raza","ALivi"};
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,Name);
+
+        ListView L1 = findViewById(R.id.listview);
+        L1.setAdapter(adp);
     }
 }
